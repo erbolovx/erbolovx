6ab8e3082692e178711513a264d8692fc46ce32c
erbolovx <123114770+erbolovx@users.noreply.github.com>
2024-01-17 14:34:01 +0600

    Homework3

---
+++ project3/js/lesson.js
@@ -0,0 +1,64 @@
+const phoneInput = document.querySelector(`#phone_input`);
+const phoneButton = document.querySelector(`#phone_button`);
+const phoneResult = document.querySelector(`#phone_result`);
+
+const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.addEventListener(`click` ,  () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = `OK`
+        phoneResult.style.color = `green`
+    }else{
+        phoneResult.innerHTML = ` NOT OK`
+        phoneResult.style.color = `red`
+    }
+})
+
+const tabContents = document.querySelectorAll(`.tab_content_block`)
+const tabItems = document.querySelectorAll(`.tab_content_item`)
+const tabParent = document.querySelector(`.tab_content_items`)
+
+let indexSlide = 0
+
+const hideTabContent = () => {
+    tabContents.forEach((tabContent) => {
+        tabContent.style.display = `none`
+    })
+    tabItems.forEach((tabItem) =>{
+        tabItem.classList.remove(`tab_content_item_active`)
+    })
+}
+
+const showTabContent = (index = 0) =>{
+    tabContents[index].style.display = `block`
+    tabItems[index].classList.add(`tab_content_item_active`)
+}
+
+hideTabContent()
+showTabContent()
+
+tabParent.onclick = (event) => {
+    if (event.target.classList.contains(`tab_content_item`)){
+        tabItems.forEach((tabItem , tabIndex) => {
+            if(event.target === tabItem){
+                hideTabContent()
+                indexSlide = tabIndex
+                showTabContent(tabIndex)
+            }
+        })
+    }
+}
+
+const autoSlider = () => {
+    setInterval(() => {
+        indexSlide++
+        if (indexSlide > tabContents.length - 1) {
+            indexSlide = 0
+        }
+        hideTabContent()
+        showTabContent(indexSlide)
+    }, 3000)
+}
+autoSlider(indexSlide)
+
+
---
+++ project3/js/modal.js
@@ -0,0 +1,38 @@
+const modal = document.querySelector('.modal');
+const modalTriggerButton = document.querySelector('#btn-get');
+const modalCloseButton = document.querySelector('.modal_close');
+
+const openModal = () => {
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+};
+
+const closeModal = () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
+};
+
+// Вызов openModal сразу после определения функции
+openModal();
+
+modalTriggerButton.onclick = openModal;
+modalCloseButton.onclick = closeModal;
+
+modal.onclick = (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+};
+
+// Функция для обработки события скролла
+const scrollDown = () => {
+    if (window.scrollY + window.innerHeight + 1 >= document.documentElement.scrollHeight) {
+        openModal();
+        document.removeEventListener('scroll', scrollDown);
+    }
+};
+
+document.addEventListener('scroll', scrollDown);
+
+// Вызов openModal через 10 секунд
+setTimeout(openModal, 10000);
